cb4cf92588d0aea2e3b941a74f08bbac67502ba8
Yujie Zhu <yujiezhu@Yujies-MBP.hitronhub.home>
2021-06-13 11:56:35 -0700

    working frontend without CSS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react'
 
-function App() {
+const BASE_URL = 'http://localhost:8080/api/'
+
+class App extends Component{
+  
+  state = {
+    incomplete: [],
+    complete: [],
+    newTask: ""
+  }
+
+componentDidMount() {
+  const url = `${BASE_URL}todos`;
+  console.log(url)
+  fetch(url)
+    .then((result) => result.json())
+    .then(({incomplete, complete}) => {
+      this.setState({ incomplete, complete })
+    })
+}
+
+incompleteTodos() {
+  const { incomplete } = this.state;
+  const result = incomplete.map((task, idx) => 
+    <li key={idx}>
+      <input onChange={(e) => {this.handleRadioSelect(idx, e)}} type="radio" value={idx}/>
+      <label>{task}</label>
+    </li>
+  );
+  return <ul>{result}</ul>;
+}
+
+completeTodos() {
+  const {complete} = this.state;
+  const result = complete.map((task, idx) => <li key={idx}>{task}</li>);
+  return <ul>{result}</ul>
+}
+
+handleRadioSelect(idx, e) {
+  const {incomplete, complete} = this.state;
+  const removed = incomplete.splice(idx, 1);
+  complete.push(...removed);
+  const url = `${BASE_URL}updateTodos`;
+  fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ incomplete, complete })
+  })
+  .then(res => res.json())
+  .then(({incomplete, complete}) => {
+    this.setState({incomplete, complete})
+    e.target.checked = false;
+  })
+
+}
+
+handleChange(event) {
+  this.setState({ newTask: event.target.value });
+}
+
+handleClick() {
+  const {incomplete, complete} = this.state;
+  incomplete.unshift(this.state.newTask);
+  const url = `${BASE_URL}updateTodos`;
+  fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ incomplete, complete })
+  })
+  .then(res => res.json())
+  .then(({incomplete, complete}) => {
+    this.setState({incomplete, complete, newTask: ""})
+  })
+}
+
+render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>A Crappy Todo REACT App</h1>
+      <input type="text" 
+        value={this.state.newTask} 
+        placeholder="add a task"
+        onChange={(event) => this.handleChange(event)}
+      ></input>
+      <button onClick={() => this.handleClick()}>add</button>
+      <h2>Tasks:</h2>
+      {this.incompleteTodos()}
+      <h2>Complete:</h2>
+      {this.completeTodos()}
     </div>
   );
+}
 }
 
 export default App;
